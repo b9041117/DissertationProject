a1694fc0b4a6ea95f960ea69f45a2ee353f37e7c
MattL <b9041117@hallam.shu.ac.uk>
2024-01-31 10:16:39 +0000

    Add project files.

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace DevelopmentProject.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DevelopmentProject.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
