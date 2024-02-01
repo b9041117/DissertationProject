192b1e64b787893d03224bda46371127fcfdcb79
MattL <b9041117@hallam.shu.ac.uk>
2024-02-01 10:58:24 +0000

    Added NotifyPropertyChanged To ViewModelBase

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -1,7 +1,14 @@
 ﻿using ReactiveUI;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace DevelopmentProject.ViewModels;
 
 public class ViewModelBase : ReactiveObject
 {
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged([CallerMemberName] string? name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 }
