e5e5682a83e4dee258f2bebd5043a5b1337e05a9
MattL <b9041117@hallam.shu.ac.uk>
2024-02-06 12:11:49 +0000

    Adding river and graphs vm to MainWindowVM

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace DevelopmentProject.ViewModels;
+﻿using DevelopmentProject.ViewModels.Graphs;
+using DevelopmentProject.ViewModels.RiverModel;
+using DevelopmentProject.Views.RiverModelView;
+
+namespace DevelopmentProject.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     public string Greeting => "Welcome to Avalonia!";
+
+    public RiverViewModel riverViewModel{ get; set; }
+    public GraphsViewModel graphsViewModel { get; set; }
+
+    public MainViewModel()
+    {
+        riverViewModel = new RiverViewModel();
+        graphsViewModel = new GraphsViewModel();
+    }
 }
