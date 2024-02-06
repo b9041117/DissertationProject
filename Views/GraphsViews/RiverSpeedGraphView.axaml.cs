20660273cd094bbb72f9398ef169c4265b8dff9c
MattL <b9041117@hallam.shu.ac.uk>
2024-02-06 12:12:18 +0000

    Creation of River and Graph VMs

---
+++ Views/GraphsViews/RiverSpeedGraphView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DevelopmentProject.Views.Graphs_Views
+{
+    public partial class RiverSpeedGraphView : UserControl
+    {
+        public RiverSpeedGraphView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/RiverModelView/RiverModelView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DevelopmentProject.Views.RiverModelView
+{
+    public partial class RiverModelView : UserControl
+    {
+        public RiverModelView()
+        {
+            InitializeComponent();
+        }
+    }
+}
