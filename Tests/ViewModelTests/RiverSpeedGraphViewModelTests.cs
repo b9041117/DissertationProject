591a593521d727f586af431deadde162173aa89f
MattL <b9041117@hallam.shu.ac.uk>
2024-05-06 13:38:22 +0100

    Tests for RiverSpeedGraphViewModel

---
+++ Tests/ViewModelTests/RiverSpeedGraphViewModelTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevelopmentProject.ViewModels.Graphs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopmentProject.Tests.ViewModelTests
+{
+    [TestClass]
+    public class RiverSpeedGraphViewModelTests
+    {
+        private RiverSpeedGraphViewModel viewModel;
+
+        [TestInitialize]
+        public void testInitialize()
+        {
+            viewModel = new RiverSpeedGraphViewModel();
+        }
+
+        [TestMethod]
+        public void ChangesRiverSpeedMethodTest()
+        {
+            viewModel.SelectedRiverDepth = 5;
+            viewModel.ChangeRiverDepth();
+
+            Assert.IsTrue(viewModel.ValueCollection is not null);
+        }
+    }
+}
